65adfde665015f7d17b281376dc3a809eacf0cd7
asseghuh <15770718424@163.com>
2023-05-31 15:03:45 +0800

    modify Client.java code

--- java-designMode/src/main/java/com/integration/designmode/decoratormode/Client.java
+++ java-designMode/src/main/java/com/integration/designmode/decoratormode/Client.java
@@ -6,16 +6,18 @@ package com.integration.designmode.decoratormode;
  */
 public class Client {
     public static void main(String[] args) {
-
-        System.out.println("华为普通手机：");
+        //以华为手机为例进行功能扩展
+        System.out.println("华为手机：");
         Phone huawei=new HuaWeiPhone();
         huawei.ican();
+        System.out.println();
 
         System.out.println("华为5G手机：");
         PhoneDecorator huawei5G=new Phone5GDecorator(new HuaWeiPhone());
         huawei5G.ican();
+        System.out.println();
 
-        System.out.println("集成Ai的华为5G手机：");
+        System.out.println("集成ChatGPT的华为5G手机：");
         PhoneDecorator huaWeiAi=new PhoneAiDecorator(huawei5G);
         huaWeiAi.ican();
 
